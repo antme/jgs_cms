e877771b526033692155207521cdc19680a01950
tao <seakingvssunking@gmail.com>
2014-03-03 10:52:19 +0800

    add

---
+++ drupal/sites/all/themes/test/templates/block--block--4.tpl.php
@@ -0,0 +1,13 @@
+
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+    <ul>
+          <li class="partner_li">
+              <span class="span_style"><img src="/images/14.jpg"></span>
+              <span class="span_style"><img src="/images/15.jpg"></span>
+              <span class="span_style"><img src="/images/16.jpg"></span>
+              <span class="span_style"><img src="../images/17.jpg"></span>
+              <span class="span_style">各区建管署链接</span>
+              <span class="span_style"><select><option>松江区建管署</option><option>崇明区建管署</option></select></span>
+          </li>
+    </ul>
+</div>
---
+++ drupal/sites/all/themes/test/templates/block--block--5.tpl.php
@@ -0,0 +1,12 @@
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+    <div class="box2" style="border:1px solid #cdcdcd; border-top:none;width:313px;">
+        <div class="titles"><h1><?php print $block->subject ?></h1></div>
+        <ul>
+            <li style="padding-top:25px;margin-left:35px;"><span class="styles">用户名：</span><input type="text" style="height:26px;" /></li>
+            <li style="padding-top:10px;margin-left:35px;"><span class="styles">密&nbsp;&nbsp;&nbsp;&nbsp;码：</span><input type="text" style="height:26px;" /></li>
+            <li style="padding-top:10px;margin-left:45px;"><button class="btn_blue">登录</button><button class="btn_red left_55">注册</button></li>
+        </ul>
+    </div>
+</div>
+
+
