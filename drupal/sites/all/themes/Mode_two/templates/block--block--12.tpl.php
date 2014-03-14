730669e4868c8b74970cce59c9c8b9539e09b111
tao <seakingvssunking@gmail.com>
2014-03-14 17:31:33 +0800

    add file

---
+++ drupal/sites/all/themes/Mode_two/templates/block--block--12.tpl.php
@@ -0,0 +1,13 @@
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+    <div class="cloudy" style="height:auto;border:none;">
+          <iframe width="250" scrolling="no" height="25" frameborder="0" allowtransparency="true" src="http://i.tianqi.com/index.php?c=code&id=1&icon=1&wind=1&num=1"></iframe>
+    </div>
+	<div class="now_date"></div>
+	<script>
+	     var myDate = new Date();
+		 var date_txt = myDate.toLocaleDateString();
+		 $(".now_date").text(date_txt);
+	</script>
+</div>
+
+
