49b69579093460301dc43b5c8c7fa57f0cfdc1e2
dylan <lieping.chen@yulor.com>
2014-05-08 20:49:26 +0800

    for access check

--- drupal/api.php
+++ drupal/api.php
@@ -4,7 +4,7 @@
 	$timestamp = $_GET ["timestamp"];
 	$nonce = $_GET ["nonce"];
 	
-	$token = TOKEN;
+	$token = "123456_dylan";
 	$tmpArr = array (
 			$token,
 			$timestamp,
@@ -17,6 +17,7 @@
 	if ($tmpStr == $signature) {
 		echo $_GET ["echostr"];
 	} else {
+		
 	}
 
 ?>
