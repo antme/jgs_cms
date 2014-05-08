b63dfef2a6f3efc3bae2351b81ea5dbe5c215246
dylan <lieping.chen@yulor.com>
2014-05-08 20:44:52 +0800

    for access check

--- drupal/api.php
+++ drupal/api.php
@@ -1,5 +1,27 @@
-<?php 
-
-	echo $_GET["echostr"];
+<?php
+if (checkSignature ()) {
+	echo $_GET ["echostr"];
+}
+function checkSignature() {
+	$signature = $_GET ["signature"];
+	$timestamp = $_GET ["timestamp"];
+	$nonce = $_GET ["nonce"];
+	
+	$token = TOKEN;
+	$tmpArr = array (
+			$token,
+			$timestamp,
+			$nonce 
+	);
+	sort ( $tmpArr, SORT_STRING );
+	$tmpStr = implode ( $tmpArr );
+	$tmpStr = sha1 ( $tmpStr );
+	
+	if ($tmpStr == $signature) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 ?>
